c0d86afc8b0aa77d95c92a1c2ea2f6d41b15a877
milansuras <milanmidhun2@gmail.com>
2024-12-28 14:51:00 +0530

    learning_python/introduction main

---
+++ Introduction/main.py
@@ -0,0 +1,38 @@
+
+# Using print statement we can pass statement  inside the braces to print out of screen.
+print("Hello World!")
+
+# to concatenate string
+print("apple" + " " + "orange")
+
+# To take input from the user for that we can use input function
+
+input("What is your name ?")
+
+print("Hello" + " " + input("Enter your name "))
+
+#variables we can say a container for to store varying data
+
+a = 10
+print(a)
+
+#When other static type language we don't need to specify the type of the variable like (java, c++)
+#As we know python is an interpreter language and a dynamic language
+
+name = input("What is your name:  ")
+print("Hello" + " " + name)
+
+a = 5
+
+print(a)
+
+a = "hello"
+
+print(a)
+
+# to get the length of a string we can use len function
+
+b = len(name)
+print(b)
+
+
